abade3b676e059719ee3bbf85a51f255239b2736
aryansh gill <aryansh.sharma@gillbroking.com>
2023-11-09 12:55:40 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { createClient } from "contentful";
+import { PostCard } from "@/components/Post";
+import { PostTitle } from "@/components/Post";
+const client = createClient({   
+  space: process.env.CONTENTFUL_SPACE_ID,   
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN, 
+});
+
+export const metadata = {
+  title: "Blog Post",
+  description: "A demo to show Contentful and Amplify Integration"
+}
+async function Home() {
+  const entries = await client.getEntries();
+  const posts = entries.items;
+  return (
+    <>
+     <div className="w-3/4 mx-auto">
+       <div className="py-16">
+         <PostTitle>My Blog Post</PostTitle>
+       </div>
+       <div className="grid grid-cols-2 gap-8">
+         {posts.map((post) => {
+           return (
+             <PostCard
+               id={post.sys.id}
+               key={post.sys.id}
+               slug={post.fields.slug + "___" + post.sys.id}
+               title={post.fields.title}
+               createdAt={post.sys.createdAt}
+               imageUrl={post.fields.postImage.fields.file.url}
+             />
+           );
+         })}
+       </div>
+     </div>
+     </>
+  )
+}
+
+export default Home;
